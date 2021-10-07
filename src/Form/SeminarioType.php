88cb03ee2d56d803ffd073b1e5b49c418eaa448f
lugetego <lugetego123./>
2021-10-06 19:35:25 -0500

    Select2 para Seminario new

--- src/Form/SeminarioType.php
+++ src/Form/SeminarioType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+
 
 
 class SeminarioType extends AbstractType
@@ -16,13 +18,19 @@ class SeminarioType extends AbstractType
         $builder
             ->add('nombre')
             ->add('lugar')
-            ->add('hora')
+            ->add('hora', TimeType::class, [
+                // renders it as a single text box
+                'input'  => 'datetime',
+                'widget' => 'single_text',
+                'placeholder' => [
+                    'hour' => 'Hour', 'minute' => 'Minute',
+                ],
+            ])
             ->add('estatus', CheckboxType::class, [
                 'label_attr' => ['class' => 'checkbox-switch'],
                 'label' => 'Activo'
             ])
             ->add('responsables')
-
         ;
     }
 
